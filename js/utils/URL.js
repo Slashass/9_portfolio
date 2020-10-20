ccff99663a7eafbba44ddfca71cce642b7ea2cd7
Slashass <70883938+Slashass@users.noreply.github.com>
2020-10-20 10:59:39 +0300

    URL documentation

--- js/utils/URL.js
+++ js/utils/URL.js
@@ -9,28 +9,49 @@ class URL {
         this.domain = location.hostname;
     }
 
+    /**
+     * @returns grazina githubo pathname
+     */
+
+    static githubPathname() {
+        return '/18a-grupe-portfolio/';
+    };
+    /**
+     * @returns sarasas galimu DOMAIN daliu, kiek localpath tiek github
+     */
+    static DOMAINS() {
+        return '127.0.0.1',
+            'localhost',
+            'front-end-by-rimantas.github.io'
+    };
+    /**
+     * @returns grazina pilna kelia pagrindines URL dalies... protocolas+domain+port(jei yra), bei jei iskvieciama githube, tai prie grazamo URL yra pridetas projekto kelias
+     */
+    static baseURL() {
+        let url = location.origin;
+
+        if (location.hostname === URL.DOMAINS()[2]) {
+            url += URL.githubPathname();
+        }
+
+    }
+
     fullURL(path) {
         // return protocol + domain + path;
     }
 
     static isHomePage() {
-        const githubPathname = '/18a-grupe-portfolio/';
-        const DOMAINS = [
-            '127.0.0.1',
-            'localhost',
-            'front-end-by-rimantas.github.io'
-        ];
         const domain = location.hostname;
 
-        if (domain === DOMAINS[0] ||
-            domain === DOMAINS[1]) {
+        if (domain === URL.DOMAINS()[0] ||
+            domain === URL.DOMAINS()[1]) {
             // localhost
             if (location.pathname === '/') {
                 return true;
             }
         } else {
             // github
-            if (location.pathname === githubPathname) {
+            if (location.pathname === URL.githubPathname()) {
                 return true;
             }
         }
