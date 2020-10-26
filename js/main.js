412688eed461176250550ed0ee159a5dac5c8da8
Slashass <70883938+Slashass@users.noreply.github.com>
2020-10-26 11:36:52 +0200

    filter

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ import { RenderHeader } from './components/header/RenderHeader.js';
 // hero
 // about us
 // portfolio gallery
+import { portfolioData } from './data/portfolio.js';
+import { Gallery } from './components/gallery/Gallery.js';
 // our services
 import { servicesData } from './data/services.js';
 import { renderServices } from './components/services/renderServices.js';
@@ -21,6 +23,7 @@ new RenderHeader(headerData);
 // hero
 // about us
 // portfolio gallery
+new Gallery(portfolioData);
 // our services
 renderServices(servicesData);
 // our team
